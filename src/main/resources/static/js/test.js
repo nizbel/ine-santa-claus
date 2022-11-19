75a44555ab41fdb63b590ab77a790fe83b61e415
Guilherme de Deus <kingbowserii@gmail.com>
2022-11-19 16:17:32 -0300

    Making usage of letter info

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -163,11 +163,12 @@ class Game extends React.Component {
 class Letter extends React.Component {
   constructor(props) {
     super(props);
+    console.log(props);
     this.state = {
-      name: '',
-      ineClass: '',
-      gifts: '',
-      letterImagePath: ''
+      name: props.name,
+      ineClass: props.ineClass,
+      gifts: props.gifts,
+      images: props.images
     }
   }
 
@@ -175,10 +176,10 @@ class Letter extends React.Component {
     return (
       <div className="col">
         <div className="card shadow-sm">
-          <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+          {/* <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> */}
+          <img className="bd-placeholder-img card-img-top" width="100%" height="225" src={this.state.images[0]} />
           <div className="card-body">
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text">{this.state.name}</p>
             <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">
                 <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
@@ -230,11 +231,11 @@ class LettersList extends React.Component {
 
   render() {
     const letters = [];
-    
     this.state.letters.forEach(letter => {
       letters.push(
         <Letter
           name={letter.name}
+          images={letter.imagePath}
         />);
     });
 

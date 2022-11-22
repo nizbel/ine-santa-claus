a513a3be814cfd1f4927773418b8058b6f71f6eb
Guilherme de Deus <kingbowserii@gmail.com>
2022-11-22 03:36:48 -0300

    Removing unused endpoint

--- src/test/java/com/inesantaclaus/letter/LetterControllerTest.java
+++ src/test/java/com/inesantaclaus/letter/LetterControllerTest.java
@@ -2,11 +2,9 @@ package com.inesantaclaus.letter;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.stringContainsInOrder;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,29 +23,6 @@ public class LetterControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
-
-	/*
-	 * Details
-	 */
-	@Test
-	public void getDetail() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get(baseUrl).param("name", "John").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(stringContainsInOrder(Arrays.asList("name", "John"))))
-				.andExpect(content().string(stringContainsInOrder(Arrays.asList("imagePath", "/aws/random/path"))));
-	}
-	
-	@Test
-	public void getDetailLackingParameter() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get(baseUrl).accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isBadRequest());
-	}
-
-	@Test
-	public void getDetailNotFound() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get(baseUrl).param("name", "Jose").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isNotFound());
-	}
 
 	/*
 	 * Listing
